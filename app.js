0c61c44c12bd87eaa9dd0fa935395c96c6d7878a
rt03vba <rt03vba@gmail.com>
2026-03-12 20:13:22 +0700

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-const kasMasuk = 500000
-const kasKeluar = 200000
+const SUPABASE_URL = "https://bbolukhqpvsmdcceipmn.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJib2x1a2hxcHZzbWRjY2VpcG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTI4MjksImV4cCI6MjA4ODg4ODgyOX0.xeZwWLufxwZT7ELwG29Y0MaC8JHee9THUiTuqP6QFss"
 
-const ctx = document.getElementById("kasChart")
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-new Chart(ctx, {
-type: "bar",
 
-data: {
-labels: ["Kas Masuk","Kas Keluar"],
+function showPage(page){
 
-datasets: [{
-label: "Statistik Kas RT",
-data: [kasMasuk,kasKeluar]
-}]
+document.querySelectorAll(".page").forEach(p=>{
+p.style.display="none"
+})
+
+document.getElementById(page).style.display="block"
+
 }
 
-})a
+
+async function loadWarga(){
+
+const { data, error } = await supabase
+.from("warga")
+.select("*")
+.order("alamat")
+
+const list = document.getElementById("warga")
+
+list.innerHTML = ""
+
+data.forEach(w=>{
+
+const div = document.createElement("div")
+
+div.innerHTML = `
+<div style="padding:10px;border-bottom:1px solid #ddd">
+<b>${w.nama}</b><br>
+Status : ${w.status}<br>
+Alamat : ${w.alamat}
+</div>
+`
+
+list.appendChild(div)
+
+})
+
+}
+
+loadWarga()
